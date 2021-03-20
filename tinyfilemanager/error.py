796c9317bdc4585bde1ee81639a8f26b3301efce
hexatester <revolusi147id@gmail.com>
2021-03-20 15:23:41 +0700

    Prepare .error

---
+++ tinyfilemanager/error.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+
+
+class TinyFileManagerError(Exception):
+    def __init__(self, message: str):
+        super().__init__()
+        self.message = message
+
+    def __str__(self) -> str:
+        return "%s" % self.message
+
+    def __reduce__(self) -> Tuple[type, Tuple[str]]:
+        return self.__class__, (self.message,)
